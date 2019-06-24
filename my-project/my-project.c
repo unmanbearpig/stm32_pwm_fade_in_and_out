6d1e0b5dd65d8890971eb0d7ff7f1d2a940e8d7e
unmanbearpig <unmanbearpig@gmail.com>
2019-06-24 12:52:23 +0300

    Blink

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,9 +1,22 @@
-#include "api.h"
-#include "api-asm.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#define LED_PORT GPIOA
+#define LED_CLOCK RCC_GPIOA
+#define LED_GPIO GPIO11
 
 int main(void) {
-	/* add your own code */
-	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+  rcc_clock_setup_in_hsi_out_48mhz();
+  rcc_periph_clock_enable(LED_CLOCK);
+  gpio_set_mode(LED_PORT, GPIO_MODE_OUTPUT_10_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, LED_GPIO);
+
+  while(true) {
+    gpio_set(LED_PORT, LED_GPIO);
+    gpio_clear(LED_PORT, LED_GPIO);
+    for(uint32_t i = 0; i < 100; i++) {
+      __asm__("nop");
+    }
+  }
+
+  return 0;
 }
